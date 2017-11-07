d9e6eb2f00995991ce409bbdb7f13004b334f75e
jakebillings <jake@jakebillings.com>
2017-11-07 14:01:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# edu-6001 #
+
+I'm going to try and do one problem per day from the MIT 6.001 problem sets. This is the repository where I do this.
---
+++ ps0/ps0.py
@@ -0,0 +1,22 @@
+# Problem Set 1 #
+#
+# Write a program that does the following in order:
+# 1. Asks the user to enter a number "x"
+# 2. Asks the user to enter a number "y"
+# 3. Prints out number "x", raised to the power "y"
+# 4. Prints out the log (base 2) of "x"
+
+# Import logarithms from the math package
+from math import log
+
+# 1
+x = input('Please enter x: ')
+
+# 2
+y = input('Please enter y: ')
+
+# 3
+print '%s to the power of %s is %s' % (x, y, x**y)
+
+# 4
+print 'log(%s)/log(2) is %s' % (x, log(x,2))
